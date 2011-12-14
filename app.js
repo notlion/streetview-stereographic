de9e6ba50ae2a38df1445af1320247710b735931
Ryan Alexander <scloopy@gmail.com>
2011-12-13 21:13:29 -0800

    Use process.env.PORT

--- app.js
+++ app.js
@@ -44,4 +44,7 @@ app.get("/pano/:id/:zoom/:x/:y", function(req, res){
 
 });
 
-app.listen(3000);
+var port = process.env.PORT || 1337;
+app.listen(port, function(){
+    console.log("Listening on port " + port);
+});
